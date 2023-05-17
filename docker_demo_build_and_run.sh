261de8bd61810026dc9bc61747c737d8010da1e4
Christoph Pilz <christoph.pilz@v2c2.at>
2023-05-17 12:48:28 +0200

    Update docker_demo_build_and_run.sh
    [fix]
    - missing network creation in docker demo

--- docker_demo_build_and_run.sh
+++ docker_demo_build_and_run.sh
@@ -1,2 +1,3 @@
 docker build -t vehicle_captain/cpp_receiver_client:0.1 -f Dockerfiles/Dockerfile.demo .
+docker network create vc-routing-bridge
 docker run -t -d --name cpp_receiver-demo --network=routing-bridge vehicle_captain/cpp_receiver_client:0.1
